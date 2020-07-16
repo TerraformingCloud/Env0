7cadb8eaa5825d0498450dd4e2e7c5f131551828
Vamsikrishna Burugadda <65275624+TerraformingCloud@users.noreply.github.com>
2020-07-16 09:40:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -29,7 +29,7 @@ variable "tenant_id" {
 variable "resource_group_name" {
     description =   "Name of the resource group"
     type        =   string
-    default     =   "Collabnix-RG"
+    default     =   "Env0-RG"
 }
 
 variable "location" {
@@ -41,7 +41,7 @@ variable "location" {
 variable "virtual_network_name" {
     description =   "Name of the virtual network"
     type        =   string
-    default     =   "Collabnix-Vnet"
+    default     =   "Env0-Vnet"
 }
 
 variable "vnet_address_range" {
